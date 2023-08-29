b961b97b1c04d8aceb05b5cea5c041693cabf8c4
bibek977 <bhattaraibibek91@gmail.com>
2023-08-29 15:07:28 +0545

    template include tag

--- sample_app/views.py
+++ sample_app/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'sample_app/index.html')
+    return render(request, 'sample_app/index.html',{'link':'Features'})
